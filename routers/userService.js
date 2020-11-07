68c60db4cc12c4d00073f0f2a0ab50cbc3302b74
THARAK <tharak1991@gmail.com>
2020-11-08 03:51:58 +0530

    updates

--- routers/userService.js
+++ routers/userService.js
@@ -1,27 +1,58 @@
 var express = require('express');
-var router = express.Router()
-const apiAdapter = require('./apiAdapter')
-const isAuthorized = require('../controller/requestAuthenticator')
+var router = express.Router();
+const axios = require('axios');
+const apiAdapter = require('./apiAdapter');
+const isAuthorized = require('../controller/requestAuthenticator');
 
-const BASE_URL = 'http://localhost:3000'
+const BASE_URL = 'http://localhost:3001'
 const api = apiAdapter(BASE_URL)
 
-router.post('/user', (req, res) => {
-  api.get(req.path).then(resp => {
-    res.send(resp.data)
-  })
+router.post('/user/', async (req, res) => {
+  try {
+    const path = req.path;
+    const apiURl = BASE_URL + path;
+
+    let resp = await axios.post(apiURl, req.body);
+
+    await res.status(201).json({
+      status: true,
+      user: resp.data
+    });
+  } catch (error) {
+    console.error(error);
+  }
 })
 
-router.get('/user/:id', isAuthorized, (req, res) => {
-  api.get(req.path).then(resp => {
-    res.send(resp.data)
-  })
+router.get('/user/:id', async (req, res) => {
+  try {
+    const path = req.path;
+    const apiURl = BASE_URL + path;
+
+    let resp = await axios.get(apiURl, req.body);
+
+    await res.status(201).json({
+      status: true,
+      user: resp.data
+    });
+  } catch (error) {
+    console.error(error);
+  }
 })
 
-router.post('/user/all', isAuthorized, (req, res) => {
-  api.post(req.path, req.body).then(resp => {
-    res.send(resp.data)
-  })
-})
+router.get('/user/all', isAuthorized, (req, res) => {
+  try {
+    const path = req.path;
+    const apiURl = BASE_URL + path;
+
+    let resp = await axios.get(apiURl, req.body);
+
+    await res.status(201).json({
+      status: true,
+      user: resp.data
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 module.exports = router
